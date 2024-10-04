439278630d2cb20defca78e9fa3a16dc2c11858b
siam51x <monirulislamsiam1010@gmail.com>
2024-10-04 19:38:43 +0600

    triangle area

---
+++ triangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+
+int a,i;
+double ans;
+printf("Enter your land number:",a);
+scanf("%d",&a);
+printf("Enter your height number:",i);
+scanf("%d",&i);
+ans=0.5*a*i;
+printf("This triangle Area is:%.2lf\n",ans);
+return 0;
+}
